418cce1df2635c4fb0d0818fdbf9ae899b95d4bc
vench <nowwrit@mail.ru>
2017-04-07 07:44:35 +0300

    Flex autoload 0407

--- src/vsapp/AutoLoad.php
+++ src/vsapp/AutoLoad.php
@@ -9,20 +9,45 @@ namespace vsapp;
  * @author vench
  */
 class AutoLoad {
+    
+    /**
+     *
+     * @var array string[]
+     */
+    private static $directoryes = [];
+
     /**
      * 
      */
     public static  function init() {
         $baseDir = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..';
-        spl_autoload_register(function ($class) use(&$baseDir) {
+        
+        if(!in_array($baseDir, self::$directoryes)) {
+            self::addDirectory($baseDir);
+        }        
+        
+        spl_autoload_register(function ($class){
             if(class_exists($class, false)) {
                 return;
             }
+            
             $path = str_replace('\\', '/', $class);
-            $file = $baseDir . DIRECTORY_SEPARATOR . $path . '.php';
-            if(file_exists($file)) {
-                include $file;
+            
+            foreach(self::$directoryes as $baseDir) {
+                $file = $baseDir . DIRECTORY_SEPARATOR . $path . '.php';
+                if(file_exists($file)) {
+                    include $file;
+                    break;
+                }                
             }    
         });
+    }
+    
+    /**
+     * 
+     * @param string $directory
+     */
+    public static function addDirectory($directory) {
+        array_push(self::$directoryes, $directory);
     }
 }
